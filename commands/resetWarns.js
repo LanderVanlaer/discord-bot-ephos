907254a526e992281d6dc9b784fd1833a9e884b9
Lander Van laer <lander.vanlaer@icloud.com>
2020-04-16 18:07:53 +0200

    let it work

--- commands/resetWarns.js
+++ commands/resetWarns.js
@@ -6,32 +6,34 @@ let warns = JSON.parse(fs.readFileSync("./jsonData/warnings.json", "utf8"));
 module.exports.run = async (bot, message, args) => {
     //!warn @daeshan <reason>
     if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`You're not allowed to use that command.`);
-    let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
-    if (!wUser) return message.reply(`User not found, use: ${pr}resetwarn @name`);
+    const wUser = message.guild.member(message.mentions.users.first() || message.guild.member(args[0]));
+    if (!wUser) return message.reply(`User not found, use: ${pr}${module.exports.help.usage}`);
 
     if (!warns[wUser.id]) {
-        message.reply(`This user was already clean`);
+        return message.reply(`This user was already clean`);
     }
 
+    const AMOUNT_OF_WARNS_REMOVED = warns[wUser.id].warns;
 
     warns[wUser.id].warns = 0;
 
-    fs.writeFile("./warnings.json", JSON.stringify(warns), (err) => {
+    fs.writeFile("./jsonData/warnings.json", JSON.stringify(warns), err => {
         if (err) console.log(err)
     });
 
-    let warnEmbed = new Discord.MessageEmbed()
-        .setAuthor("Reset Warns")
-        .setTitle("By " + message.author.username)
-        .setColor("#fc6400")
-        .addField("Cleaned User", `<@${wUser.id}> `)
-        .addField("Cleaned In", message.channel)
-        .addField("Amount of warns", warns[wUser.id].warns);
-    let warnchannel = message.guild.channels.find(x => x.name == botconfig.channels.warn || x.id == botconfig.channels.warn);
+    const warnchannel = message.guild.channels.cache.find(x => x.name == botconfig.channels.warn || x.id == botconfig.channels.warn);
     if (!warnchannel) return message.reply(`Channel "${botconfig.channels.warn}" not found, ask a mod for help!`);
 
-    warnchannel.send(warnEmbed);
-
+    warnchannel.send(
+        new Discord.MessageEmbed()
+        .setColor("#fc6400")
+        .setAuthor("Reset Warns")
+        .addField("Warns removed By", `<@${message.author.id}> `)
+        .addField("Cleaned User", `<@${wUser.id}> `)
+        .addField("Cleaned In", message.channel)
+        .addField("Amount of warns removed", AMOUNT_OF_WARNS_REMOVED)
+    );
+    message.delete().catch(console.error);
 }
 
 module.exports.help = {
