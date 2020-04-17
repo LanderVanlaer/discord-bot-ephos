afe63a468b029eae51724324337465b920f5fe97
Lander Van laer <lander.vanlaer@icloud.com>
2020-04-17 16:46:13 +0200

    Create mute.js

---
+++ extra/mute.js
@@ -0,0 +1,28 @@
+const { roles: { muteRole: roleName }, warnMuteTime = 30000 } = require("../jsonData/botconfig.json");
+const { defaultColor } = require("../jsonData/colors.json");
+
+
+module.exports = member => {
+    return new Promise(async (res, rej) => {
+        let muterole = member.guild.roles.cache.find(x => x.name == roleName);
+        if (!muterole) {
+            try {
+                await member.guild.roles.create({
+                    data: {
+                        name: roleName,
+                        color: defaultColor
+                    }
+                });
+                muterole = member.guild.roles.cache.find(x => x.name == roleName);
+                member.guild.channels.cache.forEach(async channel => {
+                    await channel.overwritePermissions([{
+                        id: muterole.id,
+                        deny: ["SEND_MESSAGES", "ADD_REACTIONS"]
+                    }]);
+                });
+            } catch (e) { rej(e) }
+        }
+        await member.roles.add(muterole);
+        res(muterole);
+    })
+}
