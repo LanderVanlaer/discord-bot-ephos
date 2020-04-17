52f09de6e4f96e80781b2a0fb211b9b9e742126c
Lander Van laer <lander.vanlaer@icloud.com>
2020-04-17 14:07:07 +0200

    displayAvatarURL to function

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -2,21 +2,21 @@ const Discord = require("discord.js");
 const botconfig = require("../jsonData/botconfig.json");
 
 module.exports.run = async (bot, message, args) => {
-  let bicon = bot.user.displayAvatarURL;
-  let botembed = new Discord.MessageEmbed()
-    .setTitle("Bot Information")
-    .setColor("#15f153")
-    .setThumbnail(bicon)
-    .addField("Bot Name", bot.user.username)
-    .addField("Created On", bot.user.createdAt)
-    .addField("Created By", "<@295203224271454208>")
-    .addField("Prefix", botconfig.prefix);
-  message.channel.send(botembed);
+    let bicon = bot.user.displayAvatarURL();
+    let botembed = new Discord.MessageEmbed()
+        .setTitle("Bot Information")
+        .setColor("#15f153")
+        .setThumbnail(bicon)
+        .addField("Bot Name", bot.user.username)
+        .addField("Created On", bot.user.createdAt)
+        .addField("Created By", "<@295203224271454208>")
+        .addField("Prefix", botconfig.prefix);
+    message.channel.send(botembed);
 }
 
 module.exports.help = {
-  name: "botinfo",
-  description: "give some info about the bot",
-  administrator: false,
-  usage: "botinfo"
+    name: "botinfo",
+    description: "give some info about the bot",
+    administrator: false,
+    usage: "botinfo"
 }
