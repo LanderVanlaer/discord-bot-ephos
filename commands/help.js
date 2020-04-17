9553aa821d18e9943f12464e744a2868d9462312
Lander Van laer <lander.vanlaer@icloud.com>
2020-04-17 13:59:30 +0200

    Update help.js
    shorting variables

--- commands/help.js
+++ commands/help.js
@@ -1,20 +1,19 @@
-const Discord = require("discord.js");
-const BOT_CONFIG = require("../jsonData/botconfig.json");
+const { MessageEmbed } = require("discord.js");
+const { prefix } = require("../jsonData/botconfig.json");
 
 module.exports.run = async (bot, message, args) => {
-    let bicon = bot.user.displayAvatarURL;
-    let botembed = new Discord.MessageEmbed()
+    const botembed = new MessageEmbed()
         .setTitle("commands")
-        .setDescription("@userNN = user Not Necessary")
-        .addField("\u200B", "\u200B")
+        .setDescription("```css\n@userNN = user Not Necessary```")
+        // .addField("\u200B", "\u200B")
         .setColor("#15f153")
-        .setThumbnail(bicon);
+        .setThumbnail(bot.user.displayAvatarURL());
     const MEMBER_HAS_ADMINISTRATOR = message.member.hasPermission("ADMINISTRATOR");
     bot.commands.forEach(cmd => { //run, name, description, administrator
         if (cmd.help.administrator) {
             if (MEMBER_HAS_ADMINISTRATOR)
-                botembed.addField(`__**${cmd.help.name}**__`, `${cmd.help.description}:\n\`${BOT_CONFIG.prefix}${cmd.help.usage}\``);
-        } else botembed.addField(`__**${cmd.help.name}**__`, `${cmd.help.description}:\n\`${BOT_CONFIG.prefix}${cmd.help.usage}\``);
+                botembed.addField(`__**${cmd.help.name}**__`, `${cmd.help.description}:\n\`${prefix}${cmd.help.usage}\``);
+        } else botembed.addField(`__**${cmd.help.name}**__`, `${cmd.help.description}:\n\`${prefix}${cmd.help.usage}\``);
     });
     message.channel.send(botembed);
 }
