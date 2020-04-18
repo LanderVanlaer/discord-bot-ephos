07aee00f8a8792ec6701313437faf5f7779a8c62
Lander Van laer <lander.vanlaer@icloud.com>
2020-04-18 13:40:11 +0200

    Create lock.js

---
+++ commands/lock.js
@@ -0,0 +1,41 @@
+const { MessageEmbed } = require("discord.js");
+const { channels: { logbook } } = require("../jsonData/botconfig.json");
+const { DARK_RED } = require("../jsonData/colors.json");
+
+module.exports.run = async (bot, message, args) => {
+    if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`You're not allowed to use that command`);
+    const { channel } = message;
+
+    try {
+        await channel.overwritePermissions([{
+            id: message.guild.roles.everyone.id,
+            deny: [
+                "SEND_MESSAGES",
+                "ADD_REACTIONS"
+            ]
+        }]);
+    } catch (error) {
+        console.error(error)
+    }
+    channel.send(`Channel <#${channel.id}> is locked`);
+    message.delete().catch(console.error);
+
+    const logbookChannel = message.guild.channels.cache.find(x => x.name == logbook || x.id == logbook);
+    if (!logbookChannel) return message.reply(`Channel "${logbook}" not found, ask a mod for help!`);
+
+    logbookChannel.send(
+        new MessageEmbed()
+        .setColor(DARK_RED)
+        .setAuthor("Channel lock")
+        .addField("Channel locked by", `<@${message.author.id}> `)
+        .addField("Locked Channel", `<#${channel.id}> `)
+        .setThumbnail(message.author.displayAvatarURL())
+    );
+}
+
+module.exports.help = {
+    name: "lock",
+    description: "Lock this channel",
+    administrator: true,
+    usage: "lock"
+}
