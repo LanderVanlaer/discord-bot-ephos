7f1f99947bd458a6be7d0eac852cd6db2fc5a39f
Lander Van laer <lander.vanlaer1@gmail.com>
2020-05-13 18:14:40 +0200

    cache problem warning file

--- commands/warnlevel.js
+++ commands/warnlevel.js
@@ -1,12 +1,12 @@
 const { maxWarns } = require("../jsonData/botconfig.json");
 const fs = require("fs");
-let warns = JSON.parse(fs.readFileSync("./jsonData/warnings.json", "utf8"));
 
 module.exports.run = async (bot, message, args) => {
     //if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("U hebt hier geen rechten voor!");
     let wUser = message.guild.member(message.mentions.users.first());
     if (!wUser) return message.reply("That user hasn't been signaled to us yet, or there's a spelling error");
 
+    const warns = JSON.parse(fs.readFileSync("./jsonData/warnings.json", "utf8"));
     let warnlevel = warns[wUser.id];
     if (!warnlevel) return message.reply("That user hasn't been signaled to us yet, or there's a spelling error");
     message.reply(`<@${wUser.id}> has ${warnlevel.warns} warnings. The max of warns is ${maxWarns}`);
